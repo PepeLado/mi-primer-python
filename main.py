33324ba9d8c617955cd28c31be90d8cb7ef694d0
PepeLado <126116472+PepeLado@users.noreply.github.com>
2024-02-08 17:50:17 -0300

    Se agrega una linea al archivo main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Bicicleta import Bicicleta
+from Bicicleta_Electrica import Bicicleta_Electrica
 
 una_bici = Bicicleta("Acme", True, "Rojo", 28)
 otra_bici = Bicicleta("Legnano", False, "Verde", 26)
+bici_elec = Bicicleta_Electrica(("BMW", True, "Azul", 22), "VW", "35cv") 
